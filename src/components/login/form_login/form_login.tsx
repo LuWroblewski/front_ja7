0b04da12d731126ccd81f5389deab4ccbb1080ff
LuWroblewski <wrolucas@gmail.com>
2024-04-16 00:28:58 -0300

    add toastify

--- src/components/login/form_login/form_login.tsx
+++ src/components/login/form_login/form_login.tsx
@@ -6,7 +6,10 @@ import { signIn } from 'next-auth/react';
 
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from 'react-hook-form';
-import { authSchema } from '../../../../validations/auth-validation';
+import { auth_schema } from '../../../../validations/auth_validation';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 type Inputs = {
   email: string;
@@ -15,7 +18,6 @@ type Inputs = {
 
 export default function Form_login() {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
 
   const router = useRouter();
 
@@ -23,7 +25,10 @@ export default function Form_login() {
     register,
     handleSubmit: onSubmit,
     formState: { errors },
-  } = useForm<Inputs>({ resolver: yupResolver(authSchema) });
+  } = useForm<Inputs>({ resolver: yupResolver(auth_schema) });
+
+  const notifySucess = () => toast.success('Login feito com sucesso.');
+  const notifyError = () => toast.error('Autenticação falhou, tente novamente.');
 
   const handleSubmit = async (data: any) => {
     setIsLoading(true);
@@ -36,34 +41,38 @@ export default function Form_login() {
 
     if (result?.error) {
       setIsLoading(false);
-      setError('Autenticação falhou, tente novamente.');
+      notifyError();
     } else {
+      notifySucess();
       router.push('/menu');
     }
   };
 
   return (
-    <form className='card-body' action='#' method='POST' onSubmit={onSubmit(handleSubmit)}>
-      <div className='form-control'>
-        <label className='label'>
-          <span className='label-text'>Email</span>
-        </label>
-        <input {...register('email')} type='email' placeholder='email' className='input input-bordered' />
-        <small className=' text-warning  p-0.5'>{errors?.email?.message}</small>
-      </div>
-      <div className='form-control'>
-        <label className='label'>
-          <span className='label-text'>Senha</span>
-        </label>
-        <input {...register('password')} type='password' placeholder='senha' className='input input-bordered' />
-        <small className=' text-warning p-0.5'>{errors?.password?.message}</small>
-      </div>
-      <small className=' text-warning p-0.5 text-center'>{error}</small>
-      <div className='form-control mt-6'>
-        <button className='btn btn-primary'>
-          {isLoading ? <span className='loading loading-spinner loading-xs'></span> : 'Entrar'}
-        </button>
-      </div>
-    </form>
+    <>
+      <form className='card-body' action='#' method='POST' onSubmit={onSubmit(handleSubmit)}>
+        <div className='form-control'>
+          <label className='label'>
+            <span className='label-text'>Email</span>
+          </label>
+          <input {...register('email')} type='email' placeholder='email' className='input input-bordered' />
+          <small className=' text-warning  p-0.5'>{errors?.email?.message}</small>
+        </div>
+        <div className='form-control'>
+          <label className='label'>
+            <span className='label-text'>Senha</span>
+          </label>
+          <input {...register('password')} type='password' placeholder='senha' className='input input-bordered' />
+          <small className=' text-warning p-0.5'>{errors?.password?.message}</small>
+        </div>
+        <div className='form-control mt-6'>
+          <button className='btn btn-primary'>
+            {isLoading ? <span className='loading loading-spinner loading-xs'></span> : 'Entrar'}
+          </button>
+        </div>
+      </form>
+
+      <ToastContainer position='top-right' autoClose={3000} theme='colored' />
+    </>
   );
 }
