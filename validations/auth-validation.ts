67325c09037854bcfd5fb0c0dc1a968eb5cf1ddd
LuWroblewski <wrolucas@gmail.com>
2024-04-16 00:28:14 -0300

    change name

--- validations/auth-validation.ts
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-const authSchema = yup.object().shape({
-  email: yup.string().email('Esse não é um email valido.').required('Email é obrigatório.').strict(true).trim(),
-  password: yup.string().required('Senha é obrigatório.').strict(true).trim(),
-});
-
-export { authSchema };
