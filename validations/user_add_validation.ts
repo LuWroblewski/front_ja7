9884e98e07da367dae50ff854beb3752a6711532
LuWroblewski <wrolucas@gmail.com>
2024-04-16 00:27:13 -0300

    add user schema

---
+++ validations/user_add_validation.ts
@@ -0,0 +1,24 @@
+import * as yup from 'yup';
+
+const user_schema = yup.object().shape({
+  first_name: yup.string().required('Nome é obrigatório').strict(true).trim(),
+  last_name: yup.string().required('Sobrenome é obrigatório').strict(true).trim(),
+  email: yup.string().email('Esse não é um email valido').required('Email é obrigatório').strict(true).trim(),
+  password: yup
+    .string()
+    .required('Senha é obrigatório')
+    .strict(true)
+    .trim()
+    .matches(/[!@#$%^&*(),.?":{}|<>]/, 'Senha deve conter caracteres especiais')
+    .min(6, 'Senha deve ter pelo menos 6 caracteres')
+    .max(20, 'Senha não deve ter mais que 20 caracteres'),
+  confirm_password: yup
+    .string()
+    .oneOf([yup.ref('password')], 'As senhas precisam ser iguais')
+    .required('Confirmação de senha é obrigatória')
+    .strict(true)
+    .trim(),
+  role: yup.string().required('Cargo é obrigatório').strict(true).trim(),
+});
+
+export { user_schema };
