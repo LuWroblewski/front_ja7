7091d1dc35b6b2bcc3d9cc85823c0f6fad21996a
LuWroblewski <wrolucas@gmail.com>
2024-04-16 00:28:26 -0300

    fix menu

--- src/components/mains/menu/menu_bar_start/menu_bar.tsx
+++ src/components/mains/menu/menu_bar_start/menu_bar.tsx
@@ -11,9 +11,9 @@ export default function Menu_bar_start() {
           <HiMenuAlt2 />
         </label>
       </div>
-      <div className='drawer-side'>
+      <div className='drawer-side z-50'>
         <label htmlFor='my-drawer' aria-label='close sidebar' className='drawer-overlay'></label>
-        <ul className='menu p-4 w-80 min-h-full bg-base-200 text-base-content'>
+        <ul className='menu p-4 w-80 min-h-full bg-base-200 text-base-content '>
           <li>
             <Link href='/menu'>
               <IoHomeOutline className='text-xl' /> Menu
