e2da3b1a745c34a30653dd4acc39886b3284b6fa
LuWroblewski <wrolucas@gmail.com>
2024-04-23 22:51:27 -0300

    att toastfy

--- toastify/index.ts
+++ toastify/index.ts
@@ -4,5 +4,9 @@ import 'react-toastify/dist/ReactToastify.css';
 export const user_notify_sucess = () => toast.success('Login feito com sucesso.');
 export const user_notify_error = () => toast.error('Autenticação falhou, tente novamente.');
 
-export const create_notify_sucess = () => toast.success('Usuário criado com sucesso.');
-export const create_notify_error = () => toast.error('Criação do usuário falhou, tente novamente.');
+export const user_crud_notify_sucess = (text: string) => toast.success(text);
+export const user_crud_notify_error = (text: string) => toast.error(text);
+
+//texto base para crud de usuarios
+//('Usuário criado com sucesso.');
+//('Criação do usuário falhou, tente novamente.');
