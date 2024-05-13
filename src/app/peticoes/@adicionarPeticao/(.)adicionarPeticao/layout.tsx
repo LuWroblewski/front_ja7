815d0f123316a9360bed136d62a2159a9550e99c
LuWroblewski <wrolucas@gmail.com>
2024-05-13 17:31:07 -0300

    dell layouttsx

--- src/app/peticoes/@adicionarPeticao/(.)adicionarPeticao/layout.tsx
+++
@@ -1,5 +0,0 @@
-import Session_auth_admin from '@/components/session/session_auth_admin/session_auth_admin';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return <>{children}</>;
-}
