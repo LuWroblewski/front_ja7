3de4021d9206242ee2165c4a35def8eb9c71ca56
LuWroblewski <wrolucas@gmail.com>
2024-05-27 17:40:59 -0300

    add logo

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,6 +12,8 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        'light-logo': "url('./images/light_logo.png')",
+        'dark-logo': "url('./images/dark_logo.png')",
       },
     },
   },
